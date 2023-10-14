5be9257ec92919922f81a7be557f1f21f7358238
Kareem Eladl <kareemo02@yahoo.com>
2023-10-15 00:23:25 +0300

    req 37

--- react-app/clinic-frontend/src/App.js
+++ react-app/clinic-frontend/src/App.js
@@ -1,53 +1,54 @@
 import logo from "./logo.svg";
-import {Route, Routes,Navigate } from 'react-router-dom';
-import PrivateRoute from './PrivateRoute';
+import { Route, Routes, Navigate } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
 import FamilyMembers from "./Components/viewFamilyMembers";
 import RegisterFamilymember from "./Components/RegistrationForm";
-import {Login} from "./Components/login";
-import {PatientHome} from "./Components/patientHome";
-import {DoctorHome} from "./Components/doctorHome";
-import {AdminHome} from "./Components/adminHome";
-import {VerifyUser} from "./Components/verifyUser";
-import {PatientRegister} from "./Components/patientRegister";
+import { Login } from "./Components/login";
+import { PatientHome } from "./Components/patientHome";
+import { DoctorHome } from "./Components/doctorHome";
+import { AdminHome } from "./Components/adminHome";
+import { VerifyUser } from "./Components/verifyUser";
+import { PatientRegister } from "./Components/patientRegister";
 import Appointments from "./Components/appointments";
 import Prescriptions from "./Components/prescriptions";
 import DoctorSearch from "./Components/searchDoctors";
+import DoctorList from "./Components/viewAllDoctors";
 
 function App() {
   return (
     <div className="App">
-          <Routes>
-            {/* public routes */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Navigate to="/login" />} />
-            <Route path="/PatientRegister" element={<PatientRegister />} />
-            
-            {/* private routes */}
-            <Route element={<PrivateRoute/>} >
-              {/* patient routes */}
-              <Route path="/patientHome" element={<PatientHome />} />
-              <Route path="/familyMembers" element={<FamilyMembers />} />
-              <Route path="/registerFamilyMember" element={<RegisterFamilymember />} />
-              <Route path="/appointments" element={<Appointments />} />
-              <Route path="/prescriptions" element={<Prescriptions />} />
-              <Route path="/searchDoctors" element={<DoctorSearch />} />
+      <Routes>
+        {/* public routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/PatientRegister" element={<PatientRegister />} />
 
-              {/* doctor routes */}
-              <Route path="/doctorhome" element={<DoctorHome />} />
+        {/* private routes */}
+        <Route element={<PrivateRoute />}>
+          {/* patient routes */}
+          <Route path="/patientHome" element={<PatientHome />} />
+          <Route path="/familyMembers" element={<FamilyMembers />} />
+          <Route
+            path="/registerFamilyMember"
+            element={<RegisterFamilymember />}
+          />
+          <Route path="/appointments" element={<Appointments />} />
+          <Route path="/prescriptions" element={<Prescriptions />} />
+          <Route path="/searchDoctors" element={<DoctorSearch />} />
 
+          {/* doctor routes */}
+          <Route path="/doctorhome" element={<DoctorHome />} />
 
-              {/* admin routes */}
-              <Route path="/adminHome" element={<AdminHome />} />
+          {/* admin routes */}
+          <Route path="/adminHome" element={<AdminHome />} />
 
+          {/* common routes */}
+          <Route path="/verifyUser" element={<VerifyUser />} />
+        </Route>
 
-              {/* common routes */}
-              <Route path="/verifyUser" element={<VerifyUser />} />
-             </Route>
-              
-            {/* not found page */}
-            <Route path="*" element={<h1>Not Found</h1>} />
-            
-          </Routes>
+        {/* not found page */}
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
     </div>
   );
 }
