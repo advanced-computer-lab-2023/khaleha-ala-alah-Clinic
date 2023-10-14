d47d33eebd4a2218cb7bf5862c885a51f558dc30
magdy hussien <modyhussien106@gmail.com>
2023-10-14 17:32:22 +0300

    new react

--- react-app/clinic-frontend/src/App.js
+++ react-app/clinic-frontend/src/App.js
@@ -1,9 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import FamilyMembers from "./Components/viewFamilyMembers";
+import RegisterFamilymember from "./Components/RegistrationForm";
 import Appointments from "./Components/appointments";
 import Prescriptions from "./Components/prescriptions";
 function App() {
-  return <Prescriptions />;
+  return <RegisterFamilymember />;
 }
 export default App;
