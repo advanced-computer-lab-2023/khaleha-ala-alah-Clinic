4072a80af98749e4a5214ec77fd45b09c8ff16bc
Abdelrahman-Elgoharyy <125679656+Abdelrahman-Elgoharyy@users.noreply.github.com>
2023-11-08 01:12:51 +0200

    removing conflicts

--- server/controllers/adminstratorController.js
+++ server/controllers/adminstratorController.js
@@ -2,9 +2,9 @@ const Admin = require("../models/users/adminModel");
 const Patient = require("../models/users/patientModel"); // Replace with the appropriate model
 const Doctor = require("../models/users/doctorModel");
 const User = require("../models/users/user");
-const bcrypt = require('bcrypt');
-const Grid = require('gridfs-stream');
-const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const Grid = require("gridfs-stream");
+const mongoose = require("mongoose");
 const { sendEmail } = require("../utilities/emails");
 const user = require("../models/users/user");
 
@@ -28,9 +28,9 @@ exports.getAllAdmins = async function (req, res) {
 
 // Create a new admin
 exports.addAdmin = async (req, res) => {
-  try{
-    const{username,password}=req.body;
-    
+  try {
+    const { username, password } = req.body;
+
     if (!username || !password) {
       return res.status(400).json({ error: "Enter all fields." });
     }
@@ -48,7 +48,7 @@ exports.addAdmin = async (req, res) => {
       password: hashedPassword,
       role: "admin",
       name: username,
-      verified: true
+      verified: true,
     });
     await user.save();
     //create admin
@@ -58,7 +58,7 @@ exports.addAdmin = async (req, res) => {
     });
     await admin.save();
     return res.status(200).json({ message: "Admin created successfully." });
-  }catch(err){
+  } catch (err) {
     console.log(err);
     return res.status(500).json({ error: "Internal server error." });
   }
@@ -109,11 +109,11 @@ exports.viewPendingDoctors = async (req, res) => {
     const pendingDoctors = await Doctor.find({ status: "pending" }).select({
       Password: 0,
       confirmPassword: 0,
-      _id: 1,
+      _id: 0,
       __v: 0,
       userID: 0,
     });
-    //console.log(pendingDoctors);
+    console.log(pendingDoctors);
     res.status(200).json({
       status: "success",
       results: pendingDoctors.length,
@@ -130,93 +130,32 @@ exports.viewPendingDoctors = async (req, res) => {
 };
 
 //approve and reject doctor
-// exports.approveDoctor = async (req, res) => {
-//   try {
-//     let admin = await Admin.findOne({ userID: req.user._id });
-//     if (!admin) {
-//       return res.status(404).json({ error: "Admin not found." });
-//     }
-//     const { type } = req.headers;
-//     if (type !== "approve" && type !== "reject") {
-//       return res.status(400).json({ error: "Invalid type specified." });
-//     }
-//     const { username } = req.body;
-//     let doctor = await Doctor.findOne({ username: username });
-//     if (!doctor) {
-//       return res.status(404).json({ error: "Doctor not found." });
-//     }
-//     type === "approve"
-//       ? (doctor.status = "accepted")
-//       : (doctor.status = "rejected");
-//     await doctor.save();
-//     doctorID = doctor.userID;
-//     doctor = await User.findOne({ _id: doctorID });
-//     type === "approve"
-//       ? (doctor.doctorApproved = true)
-//       : (doctor.doctorApproved = false);
-//     await doctor.save();
-//     return res.status(200).json({ message: `Doctor approved successfully.` });
-//   } catch (error) {
-//     return res.status(500).json({ error: "Internal server error." });
-//   }
-// };
-// accept and reject doctor request to join platform
-exports.AcceptRejectDoctor = async (req, res) => {
-  const doctorId = req.body.doctorId;
-  const action = req.body.action;
-
+exports.approveDoctor = async (req, res) => {
   try {
-    // let admin = await Admin.findOne({ userID: req.user._id });
-    // if (!admin) {
-    //   return res.status(404).json({ error: "Admin not found." });
-    // }
-    const doctor = await Doctor.findById(doctorId);
-
+    let admin = await Admin.findOne({ userID: req.user._id });
+    if (!admin) {
+      return res.status(404).json({ error: "Admin not found." });
+    }
+    const { type } = req.headers;
+    if (type !== "approve" && type !== "reject") {
+      return res.status(400).json({ error: "Invalid type specified." });
+    }
+    const { username } = req.body;
+    let doctor = await Doctor.findOne({ username: username });
     if (!doctor) {
-      return res.status(404).json({ message: "Doctor not found." });
+      return res.status(404).json({ error: "Doctor not found." });
     }
-
-    if (doctor.status !== "pending") {
-      return res.status(400).json({
-        message:
-          "Doctor request is not in pending status and cannot be processed.",
-      });
-    }
-
-    if (action === "accept") {
-      // Update the doctor's status to "accepted"
-      doctor.status = "accepted";
-      // Set the user's "doctorApproved" field to true
-      const user = await User.findOne({ _id: doctor.userID });
-      if (user) {
-        user.doctorApproved = true;
-        await user.save();
-      }
-    } else if (action === "reject") {
-      // Update the doctor's status to "rejected"
-      doctor.status = "rejected";
-    } else {
-      return res.status(400).json({ message: "Invalid action." });
-    }
-
-    await doctor.save();
-
-    return res.json({ message: `Doctor request ${action}ed.` });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err.message,
-    });
     //delete from gfs if rejected
-    if(type==="reject"){
-      gfs = new mongoose.mongo.GridFSBucket(mongoose.connection.db, { bucketName: 'uploads' });
-      const files=doctor.files;
-      files.forEach(async (file)=>{
+    if (type === "reject") {
+      gfs = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
+        bucketName: "uploads",
+      });
+      const files = doctor.files;
+      files.forEach(async (file) => {
         await gfs.delete(file);
       });
-      
     }
-    const email=doctor.email;
+    const email = doctor.email;
 
     //update status
     type === "approve"
@@ -231,17 +170,20 @@ exports.AcceptRejectDoctor = async (req, res) => {
     await doctor.save();
 
     //send email
-    const subject=type==="approve"?"Doctor Approved":"Doctor Rejected";
-    const html=type==="approve"?"<h1>Congratulations! Your account has been approved.</h1>":"<h1>Sorry! Your account has been rejected.</h1>";
-    sendEmail(email,subject,html);
+    const subject = type === "approve" ? "Doctor Approved" : "Doctor Rejected";
+    const html =
+      type === "approve"
+        ? "<h1>Congratulations! Your account has been approved.</h1>"
+        : "<h1>Sorry! Your account has been rejected.</h1>";
+    sendEmail(email, subject, html);
 
-    if(type==="approve"){
+    if (type === "approve") {
       return res.status(200).json({ message: "Doctor approved successfully." });
-    }else{
-    //delete from user and doctor
-    await user.deleteOne({username:username});
-    await Doctor.deleteOne({username:username});
-    return res.status(200).json({ message: "Doctor rejected successfully." });
+    } else {
+      //delete from user and doctor
+      await user.deleteOne({ username: username });
+      await Doctor.deleteOne({ username: username });
+      return res.status(200).json({ message: "Doctor rejected successfully." });
     }
   } catch (error) {
     console.log(error);
@@ -253,28 +195,34 @@ exports.getPendingDoctors = async (req, res) => {
   try {
     const pendingDoctors = await Doctor.find({ status: "pending" });
     const conn = mongoose.connection;
-    const gfs = new mongoose.mongo.GridFSBucket(conn.db, { bucketName: 'uploads' });
+    const gfs = new mongoose.mongo.GridFSBucket(conn.db, {
+      bucketName: "uploads",
+    });
 
-    const doctors = await Promise.all(pendingDoctors.map(async (doctor) => {
-      const downloadLinks = await Promise.all(doctor.files.map(async (file) => {
-        const attachment = await gfs.find({ _id: file }).toArray();
-        if (attachment.length > 0) {
-          const downloadLink = attachment[0].filename;
-          return downloadLink;
-        }
-        return null; 
-      }));
+    const doctors = await Promise.all(
+      pendingDoctors.map(async (doctor) => {
+        const downloadLinks = await Promise.all(
+          doctor.files.map(async (file) => {
+            const attachment = await gfs.find({ _id: file }).toArray();
+            if (attachment.length > 0) {
+              const downloadLink = attachment[0].filename;
+              return downloadLink;
+            }
+            return null;
+          })
+        );
 
-      return {
-        username: doctor.username,
-        name: doctor.name,
-        birthdate: doctor.birthdate,
-        affiliation: doctor.affiliation,
-        educationalBackground: doctor.educationalBackground,
-        speciality: doctor.speciality,
-        files: downloadLinks.filter(link => link !== null),
-      };
-    }));
+        return {
+          username: doctor.username,
+          name: doctor.name,
+          birthdate: doctor.birthdate,
+          affiliation: doctor.affiliation,
+          educationalBackground: doctor.educationalBackground,
+          speciality: doctor.speciality,
+          files: downloadLinks.filter((link) => link !== null),
+        };
+      })
+    );
 
     res.status(200).json({
       status: "success",
@@ -291,5 +239,3 @@ exports.getPendingDoctors = async (req, res) => {
     });
   }
 };
-
-
