e5d0b16bf585afb8d6df143090fe67d5ceb4e04b
Abdelrahman-Elgoharyy <125679656+Abdelrahman-Elgoharyy@users.noreply.github.com>
2023-11-09 21:43:29 +0200

    connect selected doctors with book

--- react-app/clinic-frontend/src/App.js
+++ react-app/clinic-frontend/src/App.js
@@ -29,74 +29,88 @@ import ForgotPassword from "./Components/forgotPassword";
 import NotFound from "./Components/notFound";
 import Bookpage from "./Components/Book";
 import NotApproved from "./Components/notApproved";
-import {useAuth} from "./AuthContext";
-
+import { useAuth } from "./AuthContext";
 
 function App() {
-  const {role}=useAuth();
+  const { role } = useAuth();
   return (
     <div className="App">
-          <Routes>
-            {/* public routes */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Navigate to="/login" />} />
-            <Route path="/PatientRegister" element={<PatientRegister />} />
-            <Route path="/DoctorRegister" element={<DoctorRegister />} />
-            <Route path="/resetPassword/:token" element={<ResetPassword />} />
-            <Route path="/forgotPassword" element={<ForgotPassword />} />
-            <Route path="/bookAppointment" element={<Bookpage />} />
-            {/* Redirect to login if no role is defined (user is not authenticated) */}
-            {role==="" && <Route path="*" element={<Navigate to="/login" />} />}
-            
-            {/* private routes */}
-            <Route element={<PrivateRoute/>} >
-              {/* patient routes */}
-              {role==="patient" &&(
-                <>
-                  <Route path="/patientHome" element={<PatientHome />} />
-                  <Route path="/familyMembers" element={<FamilyMembers />} />
-                  <Route path="/registerFamilyMember" element={<RegisterFamilymember />} />
-                  <Route path="/appointments" element={<Appointments />} />
-                  <Route path="/prescriptions" element={<Prescriptions />} />
-                  <Route path="/searchDoctors" element={<DoctorSearch />} />
-                  <Route path="/viewAllDoctors" element={<DoctorList />} />
-                </>              
-              )}
-             
-              {/* doctor routes */}
-              {role==="doctor" &&(
-                <>
-                  <Route path="/doctorhome" element={<DoctorHome />} />
-                  <Route path="/editDoctorProfile" element={<DoctorProfileUpdate />} />
-                  <Route path="/doctorAppointments" element={<DoctorAppointments />} />
-                  <Route path="/patientdoctorhealth" element={<PrescriptionsDoctors />} />
-                  <Route path="/viewallmypatients" element={<DoctorPatients />} />
-                </>
-              )}
+      <Routes>
+        {/* public routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/PatientRegister" element={<PatientRegister />} />
+        <Route path="/DoctorRegister" element={<DoctorRegister />} />
+        <Route path="/resetPassword/:token" element={<ResetPassword />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route path="/bookAppointment" element={<Bookpage />} />
+        {/* Redirect to login if no role is defined (user is not authenticated) */}
+        {role === "" && <Route path="*" element={<Navigate to="/login" />} />}
 
-              {/* admin routes */}
-              {role==="admin" &&(
-                <>
-                  <Route path="/adminHome" element={<AdminHome />} />
-                  <Route path="/addAdmin" element={<AddAdmin />} />
-                  <Route path="/deleteAdminDoctorPatient" element={<DeleteUser />} />
-                  <Route path="/viewPendingDoctors" element={<PendingDoctors />} />
-                  <Route path="/packages" element={<HealthPackages />} />
-                </>
-              )}
-             
+        {/* private routes */}
+        <Route element={<PrivateRoute />}>
+          {/* patient routes */}
+          {role === "patient" && (
+            <>
+              <Route path="/patientHome" element={<PatientHome />} />
+              <Route path="/familyMembers" element={<FamilyMembers />} />
+              <Route
+                path="/registerFamilyMember"
+                element={<RegisterFamilymember />}
+              />
+              <Route path="/appointments" element={<Appointments />} />
+              <Route path="/prescriptions" element={<Prescriptions />} />
+              <Route path="/searchDoctors" element={<DoctorSearch />} />
+              <Route path="/viewAllDoctors" element={<DoctorList />} />
+            </>
+          )}
 
+          {/* doctor routes */}
+          {role === "doctor" && (
+            <>
+              <Route path="/doctorhome" element={<DoctorHome />} />
+              <Route
+                path="/editDoctorProfile"
+                element={<DoctorProfileUpdate />}
+              />
+              <Route
+                path="/doctorAppointments"
+                element={<DoctorAppointments />}
+              />
+              <Route
+                path="/patientdoctorhealth"
+                element={<PrescriptionsDoctors />}
+              />
+              <Route path="/viewallmypatients" element={<DoctorPatients />} />
+            </>
+          )}
 
-              {/* common routes */}
-              {role==="notVerified" && <Route path="/verifyUser" element={<VerifyUser />} />}
-              {role==="notApproved" && <Route path="/notApproved" element={<NotApproved />} />}
+          {/* admin routes */}
+          {role === "admin" && (
+            <>
+              <Route path="/adminHome" element={<AdminHome />} />
+              <Route path="/addAdmin" element={<AddAdmin />} />
+              <Route
+                path="/deleteAdminDoctorPatient"
+                element={<DeleteUser />}
+              />
+              <Route path="/viewPendingDoctors" element={<PendingDoctors />} />
+              <Route path="/packages" element={<HealthPackages />} />
+            </>
+          )}
 
-            </Route>
-              
-            {/* not found page */}
-            <Route path="*" element={<NotFound/>} />
-            
-          </Routes>
+          {/* common routes */}
+          {role === "notVerified" && (
+            <Route path="/verifyUser" element={<VerifyUser />} />
+          )}
+          {role === "notApproved" && (
+            <Route path="/notApproved" element={<NotApproved />} />
+          )}
+        </Route>
+
+        {/* not found page */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
