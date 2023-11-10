3f7961421c41943d2c9770ccbb74e311fa7f04b2
Kareem Eladl <kareemo02@yahoo.com>
2023-11-10 02:01:25 +0200

    added name and mail from db in patient home

--- react-app/clinic-frontend/src/Components/patientHome.jsx
+++ react-app/clinic-frontend/src/Components/patientHome.jsx
@@ -10,12 +10,41 @@ import addfamilyimg from "../Images/addfmpatient.png";
 import viewdoctors from "../Images/viewdoctorpatient.png";
 import searchdoctor from "../Images/searchdoctor.png";
 import familymember from "../Images/fammember.png";
+import { useEffect } from "react";
+import axios from "axios";
 
 export const PatientHome = () => {
   const logut = () => {
     localStorage.clear();
     window.location.href = "/";
   };
+
+  const [currentPatient, setCurrentPatient] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getCurrentPatient = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/patients/currentPatient",
+          {
+            headers: {
+              // Assuming you're sending the token for authentication
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        setCurrentPatient(response.data.data.user);
+      } catch (error) {
+        console.error("Error fetching current patient", error);
+        // Handle error...
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getCurrentPatient();
+  }, []);
   const handleButtonClick = (buttonName) => {
     setActiveButton(activeButton === buttonName ? null : buttonName);
   };
@@ -27,102 +56,113 @@ export const PatientHome = () => {
   };
 
   return (
-    <div className="sidebar">
-      <div className="user-profile">
-        <img src={sidebarUserImage} alt="User" className="user-photo" />
-        <div className="user-info">
-          <p className="user-name">Adham</p>
-          <p className="user-email">adham@gmail.com</p>
-        </div>
-      </div>
-      <hr className="separator" />
-      <div className="buttons-container">
-        <Link
-          to="/appointments"
-          style={linkButtonStyle}
-          className={`sidebar-button ${
-            activeButton === "Appointments" ? "active" : ""
-          }`}
-          onClick={() => handleButtonClick("Appointments")}
-        >
-          <img src={appointmentsimg} alt="Appointments" />
-          Appointments
-        </Link>
+    <div>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <div className="sidebar">
+            <div className="user-profile">
+              <img src={sidebarUserImage} alt="User" className="user-photo" />
+              <div className="user-info">
+                <p className="user-name">{currentPatient.name}</p>
+                <p className="user-email">{currentPatient.email}</p>
+              </div>
+            </div>
+            <hr className="separator" />
+            <div className="buttons-container">
+              <Link
+                to="/appointments"
+                style={linkButtonStyle}
+                className={`sidebar-button ${
+                  activeButton === "Appointments" ? "active" : ""
+                }`}
+                onClick={() => handleButtonClick("Appointments")}
+              >
+                <img src={appointmentsimg} alt="Appointments" />
+                Appointments
+              </Link>
 
-        <Link
-          to="/prescriptions"
-          style={linkButtonStyle}
-          className={`sidebar-button ${
-            activeButton === "Prescriptions" ? "active" : ""
-          }`}
-          onClick={() => handleButtonClick("Prescriptions")}
-        >
-          <img src={perscriptionimg} alt="Perscriptions" />
-          Prescriptions
-        </Link>
+              <Link
+                to="/prescriptions"
+                style={linkButtonStyle}
+                className={`sidebar-button ${
+                  activeButton === "Prescriptions" ? "active" : ""
+                }`}
+                onClick={() => handleButtonClick("Prescriptions")}
+              >
+                <img src={perscriptionimg} alt="Perscriptions" />
+                Prescriptions
+              </Link>
 
-        <Link
-          to="/familyMembers"
-          style={linkButtonStyle}
-          className={`sidebar-button ${
-            activeButton === "FamilyMembers" ? "active" : ""
-          }`}
-          onClick={() => handleButtonClick("FamilyMembers")}
-        >
-          <img src={familymember} alt="FamilyMember" />
-          Family Members
-        </Link>
+              <Link
+                to="/familyMembers"
+                style={linkButtonStyle}
+                className={`sidebar-button ${
+                  activeButton === "FamilyMembers" ? "active" : ""
+                }`}
+                onClick={() => handleButtonClick("FamilyMembers")}
+              >
+                <img src={familymember} alt="FamilyMember" />
+                Family Members
+              </Link>
 
-        <Link
-          to="/registerFamilyMember"
-          style={linkButtonStyle}
-          className={`sidebar-button ${
-            activeButton === "RegisterFamilyMember" ? "active" : ""
-          }`}
-          onClick={() => handleButtonClick("RegisterFamilyMember")}
-        >
-          <img src={addfamilyimg} alt="Add" />
-          Add Family Members
-        </Link>
+              <Link
+                to="/registerFamilyMember"
+                style={linkButtonStyle}
+                className={`sidebar-button ${
+                  activeButton === "RegisterFamilyMember" ? "active" : ""
+                }`}
+                onClick={() => handleButtonClick("RegisterFamilyMember")}
+              >
+                <img src={addfamilyimg} alt="Add" />
+                Add Family Members
+              </Link>
 
-        <Link
-          to="/searchDoctors"
-          style={linkButtonStyle}
-          className={`sidebar-button ${
-            activeButton === "SearchDoctors" ? "active" : ""
-          }`}
-          onClick={() => handleButtonClick("SearchDoctors")}
-        >
-          <img src={searchdoctor} alt="SearchDoctors" />
-          Search Doctors
-        </Link>
+              <Link
+                to="/searchDoctors"
+                style={linkButtonStyle}
+                className={`sidebar-button ${
+                  activeButton === "SearchDoctors" ? "active" : ""
+                }`}
+                onClick={() => handleButtonClick("SearchDoctors")}
+              >
+                <img src={searchdoctor} alt="SearchDoctors" />
+                Search Doctors
+              </Link>
 
-        <Link
-          to="/viewAllDoctors"
-          style={linkButtonStyle}
-          className={`sidebar-button ${
-            activeButton === "ViewAllDoctors" ? "active" : ""
-          }`}
-          onClick={() => handleButtonClick("ViewAllDoctors")}
-        >
-          <img src={viewdoctors} alt="ViewAllDoctors" />
-          View All Doctors
-        </Link>
-        <Link
-          to="/Packages"
-          style={linkButtonStyle}
-          className={`sidebar-button ${
-            activeButton === "Packages" ? "active" : ""
-          }`}
-          onClick={() => handleButtonClick("ViewAllDoctors")}
-        >
-      
-          Packages
-        </Link>
-        {/* You can add more buttons following the same pattern */}
-      </div>
-      <div className="background-overlay"></div>
-      <img src={SidebarImage} alt="Background" className="background-image" />
+              <Link
+                to="/viewAllDoctors"
+                style={linkButtonStyle}
+                className={`sidebar-button ${
+                  activeButton === "ViewAllDoctors" ? "active" : ""
+                }`}
+                onClick={() => handleButtonClick("ViewAllDoctors")}
+              >
+                <img src={viewdoctors} alt="ViewAllDoctors" />
+                View All Doctors
+              </Link>
+              <Link
+                to="/Packages"
+                style={linkButtonStyle}
+                className={`sidebar-button ${
+                  activeButton === "Packages" ? "active" : ""
+                }`}
+                onClick={() => handleButtonClick("ViewAllDoctors")}
+              >
+                Packages
+              </Link>
+              {/* You can add more buttons following the same pattern */}
+            </div>
+            <div className="background-overlay"></div>
+            <img
+              src={SidebarImage}
+              alt="Background"
+              className="background-image"
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 };
