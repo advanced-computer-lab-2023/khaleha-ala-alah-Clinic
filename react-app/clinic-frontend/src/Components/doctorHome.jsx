98934715b6a8ec2f7d6e64b2988dcf8e608a9184
Adhamkhaledabdo <108828492+Adhamkhaledabdo@users.noreply.github.com>
2023-11-13 19:34:05 +0200

    CSS-ing the doctor's Landing Page

--- react-app/clinic-frontend/src/Components/doctorHome.jsx
+++ react-app/clinic-frontend/src/Components/doctorHome.jsx
@@ -1,42 +1,114 @@
 import React from "react";
-import "./patient.css";
 import { Link } from "react-router-dom";
+import '../Components/doctorHome.css'
+
+import makeappointment from "../Images/appointmentpatient.png";
+import chatpatient from "../Images/chat.png";
+import healthrecord from "../Images/record.png";
+import allpatients from "../Images/viewallpatients.png";
+import follow from "../Images/followup.png";
+import editprofile from "../Images/editprofile.png";
+
+
+import ImageCarousel from "../Elements/ImageCarousel";
+import ServiceItem from "../Elements/ServiceItem";
+import NavBar from "../Elements/NavBar";
+
+import appointments from "../Images/appointmentwithpatient.jpg";
+import chat from "../Images/chatwithpatient.jpg";
+import patientrecord from "../Images/patienthealth.jpg"
+import viewpatient from "../Images/viewpatient.jpg"
 
 
 export const DoctorHome = () => {
+
+
+    const slides = [
+        {
+          image: viewpatient,
+          title: "view my Patients",
+          description:
+            "Effortlessly track and manage your patient list with our comprehensive patient overview feature, all in one place.",
+        },
+        {
+          image: appointments,
+          title: "make Appointments",
+          description:
+            "Experience the ease of effortless appointment booking and simple follow-ups with your patients.",
+        },
+        {
+          image: patientrecord,
+          title: "Health Records",
+          description:
+            "Instantly access and update patient health records, ensuring accurate and up-to-date medical information.",
+        },
+        {
+          image: chat,
+          title: "Chat with your Patient",
+          description:
+            "Get immediate access to your patient with our easy-to-use chat feature. Send your personalized care anytime to your patients. SOON",
+        },
+    
+        // Add more slides as needed
+      ];
+
     return(
-        <div className="doctorHome">
-            <div>
-      {/* Navigation Bar */}
-      
+        <div>
+        <NavBar />
+        {
+          <div className="all-containers-doctorH">
+            <div className="carousel-container-doctorH">
+              <ImageCarousel slides={slides} />
+            </div>
 
-      {/* Profile Side Menu */}
-      <div className="profile-menu" id="profileMenu">
-        <a href="#">Edit My Profile</a>
-        <a href="#">Log Out</a>
+            <div className="titleofPAGE-doctorH">
+              <h1> To cure sometimes, to relieve often, to comfort always. </h1>
+            </div>
+            <div className="services-container-doctorH">
+            <ServiceItem
+                imgSrc={editprofile}
+                title="Edit My Profile"
+                description="Easily edit and update your profile details"
+                navigateTo="/editDoctorProfile"
+              />
+              ,
+              <ServiceItem
+                imgSrc={allpatients}
+                title="View My Patients"
+                description="Explore profiles and expertise of all your patients"
+                navigateTo="/viewallmypatients"
+              />
+              ,
+              <ServiceItem
+                imgSrc={makeappointment}
+                title="Make Appointments"
+                description="Schedule a consultation with your patients anytime"
+                navigateTo="/doctorAppointments"
+              />
+              ,
+              <ServiceItem
+                imgSrc={healthrecord}
+                title="Patient Health Records"
+                description="view and manage detailed health records of your patients"
+                navigateTo="/patientdoctorhealth"
+              />
+              ,
+              <ServiceItem
+                imgSrc={follow}
+                title="Follow-Up Scheduler"
+                description="Streamline patient follow-ups,ensuring timely and organized care appointments."
+                navigateTo="/follow-up-scheduler"
+              />
+              ,
+              <ServiceItem
+                imgSrc={chatpatient}
+                title="Chat with Doctor"
+                description="Connect with your doctor for instant advice through secure messaging. SOON"
+              />
+              ,
+            </div>
+          </div>
+        }
       </div>
-
-      {/* Sidebar */}
-      <div className="sidebar">
-        <div className="sidebar-buttons">
-            <Link to="/editDoctorProfile" className="sidebar-button">
-                edit profile
-            </Link>
-            <Link to="/doctorAppointments" className="sidebar-button">
-                Appointments
-            </Link>
-            <Link to="/patientdoctorhealth" className="sidebar-button">
-                patient health
-            </Link>
-            <Link to="/viewallmypatients" className="sidebar-button">
-                view my patients
-            </Link>
-            <Link to="/follow-up-scheduler" className="sidebar-button">
-                schedule a follow up
-            </Link>
-        </div>
-      </div>
-    </div>
-        </div>
     );
 };
