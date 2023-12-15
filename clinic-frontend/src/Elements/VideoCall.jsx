4e9f7406b796110218f13fb4032e7d596d58e8ed
Mahmoud Sobh <msobh71@gmail.com>
2023-12-15 20:21:23 +0200

    video styles

--- clinic-frontend/src/Elements/VideoCall.jsx
+++ clinic-frontend/src/Elements/VideoCall.jsx
@@ -1,30 +1,79 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { Modal, Button, Row, Col } from 'antd';
+import { PhoneOutlined, AudioMutedOutlined, AudioOutlined, VideoCameraOutlined, VideoCameraAddOutlined } from '@ant-design/icons';
 
-const VideoCall = ({ myVideoStream, userVideoStream ,leaveCall}) => {
+const VideoCall = ({ myVideoStream, userVideoStream, leaveCall }) => {
   const myVideoRef = useRef(null);
   const userVideoRef = useRef(null);
-
-
+  const [isAudioMuted, setIsAudioMuted] = useState(true);
+  const [isVideoMuted, setIsVideoMuted] = useState(true);
 
   useEffect(() => {
-   setTimeout(()=>{
-    console.log(userVideoStream.current);
     if (myVideoStream) {
       myVideoRef.current.srcObject = myVideoStream.current;
     }
     if (userVideoStream) {
       userVideoRef.current.srcObject = userVideoStream.current;
     }
-   },1000)
   }, [userVideoStream, myVideoStream]);
 
+  const toggleAudio = () => {
+    const audioTracks = myVideoStream.current.getAudioTracks();
+    audioTracks.forEach((track) => {
+      track.enabled = !isAudioMuted;
+    });
+    setIsAudioMuted(!isAudioMuted);
+  };
+
+  const toggleVideo = () => {
+    const videoTracks = myVideoStream.current.getVideoTracks();
+    videoTracks.forEach((track) => {
+      track.enabled = !isVideoMuted;
+    });
+    setIsVideoMuted(!isVideoMuted);
+  };
+
   return (
-    <div>
-      <h2>Video Call</h2>
-      <video ref={myVideoRef} autoPlay style={{ width: '240px', height: '180px' }}></video>
-      <video ref={userVideoRef} autoPlay style={{ width: '240px', height: '180px' }}></video>
-      <button onClick={leaveCall}>End Call</button>
-    </div>
+    <Modal
+      visible
+      footer={null}
+      closable={false}
+      maskClosable={false}
+      style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', border: 'none', padding: 0 }}
+    >
+      <Row style={{ width: '100%', height: '100%', position: 'relative' }}>
+        <Col span={24} style={{ position: 'relative' }}>
+          <video ref={userVideoRef} autoPlay style={{ width: '100%', height: '100%', objectFit: 'cover' }}></video>
+        </Col>
+        <Col span={6} style={{ position: 'absolute', bottom: '70%', right: 0 }}>
+          <video ref={myVideoRef} autoPlay muted style={{ width: '100%', height: 'auto', borderRadius: '8px' }}></video>
+        </Col>
+        <Button
+          type="primary"
+          shape="circle"
+          size="large"
+          icon={isAudioMuted ? <AudioOutlined /> : <AudioMutedOutlined />}
+          style={{ position: 'absolute', bottom: '2%', right: '40%', background: '#ff4500', border: 'none' }}
+          onClick={toggleAudio}
+        />
+        <Button
+          type="primary"
+          shape="circle"
+          size="large"
+          icon={isVideoMuted ? <VideoCameraAddOutlined /> : <VideoCameraOutlined />}
+          style={{ position: 'absolute', bottom: '2%', right: '50%', background: '#ff4500', border: 'none' }}
+          onClick={toggleVideo}
+        />
+        <Button
+          type="primary"
+          shape="circle"
+          size="large"
+          icon={<PhoneOutlined />}
+          style={{ position: 'absolute', bottom: '2%', right: '60%', background: '#ff4500', border: 'none' }}
+          onClick={leaveCall}
+        />
+      </Row>
+    </Modal>
   );
 };
 
